e713f027fbf0d5f51c9fcf5bed517a40bc431a0d
Rahul Yadav <rahulkyadav57@gmail.com>
2025-02-18 18:17:02 +0530

    fix: display FunC version instead of func-js

--- src/components/workspace/BuildProject/BuildProject.tsx
+++ src/components/workspace/BuildProject/BuildProject.tsx
@@ -38,6 +38,7 @@ import { ABIParser, parseInputs } from '@/utility/abi';
 import { extractContractName } from '@/utility/contract';
 import { filterABIFiles } from '@/utility/file';
 import { replaceFileExtension } from '@/utility/filePath';
+import { compilerVersion } from '@ton-community/func-js';
 import { Maybe } from '@ton/core/dist/utils/maybe';
 import { TonClient } from '@ton/ton';
 import { useForm } from 'antd/lib/form/Form';
@@ -62,6 +63,7 @@ interface Props {
 const BuildProject: FC<Props> = ({ projectId, contract, updateContract }) => {
   const [isLoading, setIsLoading] = useState('');
   const [buildCount, setBuildCount] = useState(0);
+  const [compilerInfo, setCompilerInfo] = useState('');
   const { createLog } = useLogActivity();
   const [environment, setEnvironment] = useState<NetworkEnvironment>('SANDBOX');
   const [buildOutput, setBuildoutput] = useState<{
@@ -98,19 +100,22 @@ const BuildProject: FC<Props> = ({ projectId, contract, updateContract }) => {
     packageJson.dependencies['@tact-lang/compiler'],
     '^',
   );
-  const funcVersion = stripPrefix(
-    packageJson.dependencies['@ton-community/func-js'],
-    '^',
-  );
-
-  const compilerVersionInfo =
-    activeProject?.language === 'tact'
-      ? `- Tact version: ${tactVersion}`
-      : `- FunC version: ${funcVersion}`;
 
   const [deployForm] = useForm();
 
   const { deployContract } = useContractAction();
+
+  const updateCompilerInfo = async () => {
+    let versionInfo;
+    if (activeProject?.language === 'tact') {
+      versionInfo = `- Tact version: ${tactVersion}`;
+    } else {
+      const funcCompilerVersion = await compilerVersion();
+      versionInfo = `- FunC version: ${funcCompilerVersion.funcVersion}`;
+    }
+
+    setCompilerInfo(versionInfo);
+  };
 
   const contractsToDeploy = () => {
     if (!activeProject?.path || !activeProject.language) {
@@ -670,6 +675,10 @@ const BuildProject: FC<Props> = ({ projectId, contract, updateContract }) => {
   }, [selectedContract, contract]);
 
   useEffect(() => {
+    updateCompilerInfo();
+  }, [activeProject]);
+
+  useEffect(() => {
     try {
       updateContractInstance().catch(() => {});
     } catch (e) {
@@ -790,8 +799,8 @@ const BuildProject: FC<Props> = ({ projectId, contract, updateContract }) => {
               isAutoBuildAndDeployEnabled()
                 ? '- Auto-build and deploy is enabled for Sandbox and can be changed in settings. <br />'
                 : ''
-            }
-            ${compilerVersionInfo}
+            } 
+            ${compilerInfo}
             `}
           allowedFile={['fc', 'tact']}
           onCompile={() => {
