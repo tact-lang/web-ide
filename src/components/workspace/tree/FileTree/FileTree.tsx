367cf442a771d24f942a91184f09a7f02fe14647
Rahul Yadav <rahulkyadav57@gmail.com>
2025-02-26 18:19:17 +0530

    feat: improve root file/folder creation UX & auto-open new file

--- src/components/workspace/tree/FileTree/FileTree.tsx
+++ src/components/workspace/tree/FileTree/FileTree.tsx
@@ -1,4 +1,10 @@
+import { useFileTab } from '@/hooks';
+import { useLogActivity } from '@/hooks/logActivity.hooks';
 import { useProject } from '@/hooks/projectV2.hooks';
+import { Tree as ITree } from '@/interfaces/workspace.interface';
+import EventEmitter from '@/utility/eventEmitter';
+import { relativePath } from '@/utility/filePath';
+import Path from '@isomorphic-git/lightning-fs/src/path';
 import {
   DropOptions,
   getBackendOptions,
@@ -6,17 +12,27 @@ import {
   NodeModel,
   Tree,
 } from '@minoru/react-dnd-treeview';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import s from './FileTree.module.scss';
 import TreeNode, { TreeNodeData } from './TreeNode';
+import TreePlaceholderInput from './TreePlaceholderInput';
 
 interface Props {
   projectId: string;
 }
+export interface INewItem {
+  name: string;
+  type: ITree['type'];
+  parentPath: string;
+}
 
 const FileTree: FC<Props> = ({ projectId }) => {
-  const { activeProject, projectFiles, moveItem } = useProject();
+  const { activeProject, projectFiles, moveItem, newFileFolder } = useProject();
+  const { open: openTab } = useFileTab();
+  const { createLog } = useLogActivity();
+
+  const [newItem, setNewItem] = useState<INewItem | null>(null);
 
   const getProjectFiles = (): NodeModel[] => {
     if (!activeProject?.path) return [];
@@ -40,6 +56,46 @@ const FileTree: FC<Props> = ({ projectId }) => {
     );
   };
 
+  const commitItemCreation = async (name?: string) => {
+    if (!newItem || !activeProject?.path) return;
+
+    const itemName = name ?? newItem.name;
+    const absolutePath = Path.join(newItem.parentPath, itemName);
+    const relativeFilePath = relativePath(absolutePath, activeProject.path);
+    try {
+      await newFileFolder(relativeFilePath, newItem.type);
+      if (newItem.type === 'file') {
+        openTab(itemName, absolutePath);
+      }
+      reset();
+    } catch (error) {
+      createLog((error as Error).message, 'error');
+    }
+  };
+
+  const reset = () => {
+    document.body.classList.remove('editing-file-folder');
+    setNewItem(null);
+  };
+
+  const handleRootItemCreation = (type: ITree['type']) => {
+    if (!activeProject?.path) return;
+    setNewItem({
+      type,
+      parentPath: activeProject.path,
+      name: '',
+    });
+  };
+
+  useEffect(() => {
+    EventEmitter.on('CREATE_ROOT_FILE_OR_FOLDER', handleRootItemCreation);
+
+    return () => {
+      reset();
+      EventEmitter.on('CREATE_ROOT_FILE_OR_FOLDER', handleRootItemCreation);
+    };
+  }, []);
+
   if (!activeProject?.path) return null;
 
   return (
@@ -49,17 +105,41 @@ const FileTree: FC<Props> = ({ projectId }) => {
           tree={getProjectFiles()}
           rootId={activeProject.path}
           onDrop={handleDrop}
-          render={(node, { depth, isOpen, onToggle }) => (
-            <TreeNode
-              projectId={projectId as string}
-              node={node as NodeModel<TreeNodeData>}
-              depth={depth}
-              isOpen={isOpen}
-              onToggle={onToggle}
-            />
-          )}
+          render={(node, { depth, isOpen, onToggle }) => {
+            return (
+              <TreeNode
+                projectId={projectId as string}
+                node={node as NodeModel<TreeNodeData>}
+                depth={depth}
+                isOpen={isOpen}
+                onToggle={onToggle}
+                newItem={newItem}
+                commitItemCreation={commitItemCreation}
+                setNewItem={(data: INewItem | null) => {
+                  if (!data) {
+                    reset();
+                    return;
+                  }
+                  const { name, type, parentPath } = data;
+                  setNewItem({
+                    name,
+                    type,
+                    parentPath,
+                  });
+                }}
+              />
+            );
+          }}
         />
       </DndProvider>
+      {newItem?.parentPath === activeProject.path && (
+        <TreePlaceholderInput
+          style={{ paddingInlineStart: 15 }}
+          onSubmit={commitItemCreation}
+          onCancel={reset}
+          type={newItem.type}
+        />
+      )}
     </div>
   );
 };
