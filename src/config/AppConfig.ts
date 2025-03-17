015aba52d0bb6ac991a1d48a4df54a4cb25a5fca
Rahul Yadav <rahulkyadav57@gmail.com>
2025-03-17 17:00:55 +0530

    chore: set env

--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 
+const NODE_ENVIRONMENT = process.env.NODE_ENV;
+
 const MIXPANEL_TOKEN =
-  process.env.REACT_APP_MIXPANEL_TOKEN?.trim() || 'unknown';
+  process.env.REACT_APP_MIXPANEL_TOKEN?.trim() ||
+  '279a0a925b2c3388797950019c3733b4';
 
 export const AppConfig = {
   name: 'TON Web IDE',
@@ -13,11 +16,14 @@ export const AppConfig = {
   analytics: {
     MIXPANEL_TOKEN,
     IS_ENABLED:
-      process.env.REACT_APP_ANALYTICS_ENABLED?.toLowerCase() === 'true' &&
+      (NODE_ENVIRONMENT === 'production' ||
+        process.env.REACT_APP_ANALYTICS_ENABLED?.toLowerCase() === 'true') &&
       MIXPANEL_TOKEN !== 'unknown',
   },
   proxy: {
-    key: process.env.REACT_APP_PROXY_KEY || '',
+    key:
+      process.env.REACT_APP_PROXY_KEY ||
+      'temp_3475bde873d9be05422a9765bdab210d',
     url: process.env.REACT_APP_PROXY_URL || 'https://proxy.cors.sh/',
   },
   cors: {
